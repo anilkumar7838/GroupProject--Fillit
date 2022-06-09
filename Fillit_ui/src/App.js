0fd8009597b467e5a41b2af800e669c47eae35db
anilkumar7838 <anilkumar9868573282@gmail.com>
2022-06-09 23:21:20 +0530

    temporary form

--- Fillit_ui/src/App.js
+++ Fillit_ui/src/App.js
@@ -5,7 +5,7 @@ import Home from "./views/Home";
 import Fill from "./views/Fill";
 import MyForms from "./views/MyForms";
 import About from "./components/About/about"
-
+import Form from "./components/RegistrationForm/form";
 function App() {
   return (
     <div className='App'>
@@ -14,6 +14,7 @@ function App() {
           <Route path='/fill' element={<Fill />} />
           <Route path='/myforms' element={<MyForms />} />
           <Route path='/' element={<Home />} />
+          <Route path='/form' element={<Form/>} />
           <Route path='/about' element={<About />} />
         </Routes>
       </Router>
